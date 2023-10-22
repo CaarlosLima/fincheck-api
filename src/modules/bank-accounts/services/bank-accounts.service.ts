d0482fa8ff4864b8f1ef03aab8828df59882e03e
Carlos Eduardo de Lima <caduu@live.com>
2023-10-22 19:06:37 -0300

    feat: add validate bank account ownership service

--- src/modules/bank-accounts/services/bank-accounts.service.ts
+++ src/modules/bank-accounts/services/bank-accounts.service.ts
@@ -1,11 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { BankAccountsRepository } from './../../shared/database/repositories/bank-accounts.repositories';
-import { CreateBankAccountDto } from './dto/create-bank-account.dto';
-import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
+import { Injectable } from '@nestjs/common';
+import { BankAccountsRepository } from 'src/shared/database/repositories/bank-accounts.repositories';
+import { CreateBankAccountDto } from '../dto/create-bank-account.dto';
+import { UpdateBankAccountDto } from '../dto/update-bank-account.dto';
+import { ValidateBankAccountOwnershipService } from './validate-bank-account-ownership.service';
 
 @Injectable()
 export class BankAccountsService {
-  constructor(private readonly bankAccountsRepo: BankAccountsRepository) {}
+  constructor(
+    private readonly bankAccountsRepo: BankAccountsRepository,
+    private readonly validateBankAccountOwnership: ValidateBankAccountOwnershipService,
+  ) {}
 
   create(userId: string, createBankAccountDto: CreateBankAccountDto) {
     const { color, initialBalance, name, type } = createBankAccountDto;
@@ -24,7 +28,7 @@ export class BankAccountsService {
     bankAccountId: string,
     updateBankAccountDto: UpdateBankAccountDto,
   ) {
-    await this.validateBankAccountOwnership(userId, bankAccountId);
+    await this.validateBankAccountOwnership.validate(userId, bankAccountId);
 
     const { color, initialBalance, name, type } = updateBankAccountDto;
 
@@ -35,25 +39,12 @@ export class BankAccountsService {
   }
 
   async delete(userId: string, bankAccountId: string) {
-    await this.validateBankAccountOwnership(userId, bankAccountId);
+    await this.validateBankAccountOwnership.validate(userId, bankAccountId);
 
     await this.bankAccountsRepo.delete({
       where: { id: bankAccountId },
     });
 
     return null;
-  }
-
-  private async validateBankAccountOwnership(
-    userId: string,
-    bankAccountId: string,
-  ) {
-    const isOwner = await this.bankAccountsRepo.findFirst({
-      where: { id: bankAccountId, userId },
-    });
-
-    if (!isOwner) {
-      throw new NotFoundException('Bank account not found.');
-    }
   }
 }
