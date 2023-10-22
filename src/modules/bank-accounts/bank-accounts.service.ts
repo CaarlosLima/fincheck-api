6e05bd32d61b036b191f3fde5bdbda211a5f4104
Carlos Eduardo de Lima <caduu@live.com>
2023-10-22 18:58:34 -0300

    feat: implement bank accounts resources

---
+++ src/modules/bank-accounts/bank-accounts.service.ts
@@ -0,0 +1,59 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BankAccountsRepository } from './../../shared/database/repositories/bank-accounts.repositories';
+import { CreateBankAccountDto } from './dto/create-bank-account.dto';
+import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
+
+@Injectable()
+export class BankAccountsService {
+  constructor(private readonly bankAccountsRepo: BankAccountsRepository) {}
+
+  create(userId: string, createBankAccountDto: CreateBankAccountDto) {
+    const { color, initialBalance, name, type } = createBankAccountDto;
+
+    return this.bankAccountsRepo.create({
+      data: { userId, color, initialBalance, name, type },
+    });
+  }
+
+  findAllByUserId(userId: string) {
+    return this.bankAccountsRepo.findMany({ where: { userId } });
+  }
+
+  async update(
+    userId: string,
+    bankAccountId: string,
+    updateBankAccountDto: UpdateBankAccountDto,
+  ) {
+    await this.validateBankAccountOwnership(userId, bankAccountId);
+
+    const { color, initialBalance, name, type } = updateBankAccountDto;
+
+    return this.bankAccountsRepo.update({
+      where: { id: bankAccountId },
+      data: { color, initialBalance, name, type },
+    });
+  }
+
+  async delete(userId: string, bankAccountId: string) {
+    await this.validateBankAccountOwnership(userId, bankAccountId);
+
+    await this.bankAccountsRepo.delete({
+      where: { id: bankAccountId },
+    });
+
+    return null;
+  }
+
+  private async validateBankAccountOwnership(
+    userId: string,
+    bankAccountId: string,
+  ) {
+    const isOwner = await this.bankAccountsRepo.findFirst({
+      where: { id: bankAccountId, userId },
+    });
+
+    if (!isOwner) {
+      throw new NotFoundException('Bank account not found.');
+    }
+  }
+}
